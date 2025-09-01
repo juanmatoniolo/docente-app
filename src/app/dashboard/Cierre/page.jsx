fa04643907ab22137cdc102bec5728680a7cadef
juanmatoniolo <juanmatoniolo2@gmail.com>
2025-09-01 02:46:01 -0300

    use client

---
+++ src/app/dashboard/Cierre/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import TermClosureWrapper from "./TermClosureWrapper";
+
+export default function CierrePage() {
+    // Server Component simple que solo carga el Client Component
+    return <TermClosureWrapper />;
+}
--- src/app/lib/rtdb.js
+++ src/app/lib/rtdb.js
@@ -1,3 +1,5 @@
+"use client";
+
 import { db } from "./firebaseClient";
 import {
 	ref,
