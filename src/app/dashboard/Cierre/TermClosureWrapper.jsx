7544e24320bfc817315d628960b26c6582b55f51
juanmatoniolo <juanmatoniolo2@gmail.com>
2025-09-01 02:35:32 -0300

    Fix Cierre page f or Vercel deployment

--- src/app/dashboard/Cierre/TermClosureWrapper.jsx
+++
@@ -1,8 +0,0 @@
-"use client"; // Obligatorio: todo lo que use document/window debe ser client
-
-import React from "react";
-import TermClosure from "../../../components/TermClosure";
-
-export default function TermClosureWrapper() {
-  return <TermClosure />;
-}
--- src/app/dashboard/Cierre/page.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import TermClosureWrapper from "./TermClosureWrapper";
-
-export default function CierrePage() {
-  // Server Component simple que solo carga el Client Component
-  return <TermClosureWrapper />;
-}
